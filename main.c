5320983d7b8ae244b0ceb119e8b660373f1677d7
franacu <fmacuna@fie.undef.edu.ar>
2023-04-14 12:13:23 -0300

    Inicianilazndo

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+void sumaproducto(int  n1,int n2,int *suma,int *producto)
+{
+    *suma=n1+n2;
+    *producto=n1*n2;
+}
+
 int main()
 {
+    int suma;
+    int producto;
+    sumaproducto(n1 var1,n2 var2,&suma,&producto)
+    printf("La suma es %d\n El producto es:%d\n",*suma,*producto);
 
     return 0;
 }
